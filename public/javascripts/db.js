09b522d0effda42c95b0a7f6acaa343f71c0b7fb
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2021-03-04 15:03:40 -0800

    Temporarily disabled all code pertaining to MongoDB

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,32 +1,32 @@
-var { MongoClient } = require("mongodb");
+// var { MongoClient } = require("mongodb");
 
-function myDB() {
-  const mydb = {};
+// function myDB() {
+//   const mydb = {};
 
-  const url = "mongodb://localhost:27017";
-  const DB_NAME = "myFiles";
+//   const url = "mongodb://localhost:27017";
+//   const DB_NAME = "myFiles";
 
-  mydb.getFiles = async (query = {}) => {
-    let client;
+//   mydb.getFiles = async (query = {}) => {
+//     let client;
 
-    try {
-      client = new MongoClient(url);
+//     try {
+//       client = new MongoClient(url);
 
-      console.log("Connecting to the db");
-      await client.connect();
-      console.log("Connected");
+//       console.log("Connecting to the db");
+//       await client.connect();
+//       console.log("Connected");
 
-      const db = client.db(DB_NAME);
-      const filesCol = db.collection("files");
+//       const db = client.db(DB_NAME);
+//       const filesCol = db.collection("files");
 
-      const query = {};
-      const files = await filesCol.find(query).toArray();
-      console.log("Got files", files);
-    } finally {
-      console.log("closing the connection");
-      client.close();
-    }
-  };
-  return mydb;
-}
-module.exports = myDB();
+//       const query = {};
+//       const files = await filesCol.find(query).toArray();
+//       console.log("Got files", files);
+//     } finally {
+//       console.log("closing the connection");
+//       client.close();
+//     }
+//   };
+//   return mydb;
+// }
+// module.exports = myDB();
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var router = express.Router();
-const myDB = require("../public/javascripts/db.js");
+// const myDB = require("../public/javascripts/db.js");
 
 /* GET home page. */
 router.get("/", function (req, res) {
   res.render("index");
 });
 
-router.get("/getFiles", async (req, res) => {
-  const files = await myDB.getFiles();
-  res.send({ files: files });
-});
+// router.get("/getFiles", async (req, res) => {
+//   const files = await myDB.getFiles();
+//   res.send({ files: files });
+// });
 
 module.exports = router;
