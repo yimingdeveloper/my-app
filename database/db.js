442d69752a9e3502c1456131802158fb5fc4081f
Hasti Gheibi Dehnashi <hastidehnashi@gmail.com>
2021-03-12 17:52:49 -0800

    deleted empty js file

--- database/db.js
+++ database/db.js
@@ -58,3 +58,25 @@ function myDB() {
   return mydb;
 }
 module.exports = myDB();
+
+// const MongoClient = require("mongodb").MongoClient;
+// const assert = require("assert");
+
+// // Connection URL
+// const url = "mongodb://localhost:27017";
+
+// // Database Name
+// const dbName = "myapp";
+
+// // Create a new MongoClient
+// const client = new MongoClient(url);
+
+// // Use connect method to connect to the Server
+// client.connect(function (err) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+
+//   const db = client.db(dbName);
+
+//   client.close();
+// });
